10d281e3675f126df775ed3972aead55dd0e4ed8
Joseph Riddle <joeriddles10@gmail.com>
2023-12-02 09:26:06 -0800

    Solve day 2 part 2

--- 2023/day2/part2.py
+++ 2023/day2/part2.py
@@ -12,7 +12,7 @@ class Tests(unittest.TestCase):
     def test_example_short(self):
         input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
         actual = main([input])
-        expected = [1]
+        expected = [48]
         assert actual == expected
 
     def test_example(self):
@@ -22,7 +22,7 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""".splitlines()
         actual = main(input)
-        expected = [1, 2, 5]
+        expected = [48, 12, 1560, 630, 36]
         assert actual == expected
     
     def test_empty(self):
@@ -32,19 +32,7 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""".splitlines()
 @dataclasses.dataclass
 class Game:
     id: int
-    plays: list[dict[str, int]]
-
-    def get_possible(self, max: dict[str, int]) -> bool:
-        possible = True
-
-        for play in self.plays:
-            for key, play_val in play.items():
-                max_val = max[key]
-                if play_val > max_val:
-                    possible = False
-                    break
-
-        return possible
+    plays: list[list[tuple[int, str]]]
 
     @classmethod
     def from_str(cls, input: str) -> Game:
@@ -54,34 +42,35 @@ class Game:
         id = int(game_str.split()[-1]) # there is a cleaner way of doing this...
         
         play_strs = [play_str.strip() for play_str in play_str.split(";")]
-        play_tuples: list[list[tuple[int, str]]] = []
+        plays: list[list[tuple[int, str]]] = []
         for play_str in play_strs:
             move_strs = [move_str.strip() for move_str in play_str.split(",")]
             moves_for_play = [(int(splt[0]), splt[1]) for move_str in move_strs if (splt := move_str.split())]
-            play_tuples.append(moves_for_play)
-
-        plays: list[dict[str, int]] = []
-        for moves in play_tuples:
-            play = collections.defaultdict(lambda: 0)
-            for num, color in moves:
-                play[color] += num
-            plays.append(play)
+            plays.append(moves_for_play)
 
         return Game(id, plays)
     
 
 def main(input: list[str]) -> list[int]:
-    max: dict[str, int] = {
-        "red": 12,
-        "green": 13,
-        "blue": 14,
-    }
-    possible_game_ids = []
-    for line in input:
-        game = Game.from_str(line)
-        if game.get_possible(max):
-            possible_game_ids.append(game.id)
-    return possible_game_ids
+    games = [Game.from_str(line) for line in input]
+
+    games_maxs: list[dict[str, int]] = []
+    for game in games:
+        max_num_by_color: dict[str, int] = collections.defaultdict(lambda: 0)
+        for play in game.plays:
+            for num, color in play:
+                if num > max_num_by_color[color]:
+                    max_num_by_color[color] = num
+        games_maxs.append(max_num_by_color)
+    
+    powers: list[int] = []
+    for game_max in games_maxs:
+        power = 1
+        for num in game_max.values():
+            power *= num
+        powers.append(power)
+
+    return powers
 
 
 if __name__ == "__main__":
