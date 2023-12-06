05f27e716577d60b0f848006af29f2e0daaf212f
jriddle <jriddle@axon.com>
2023-12-05 21:40:41 -0800

    Solve day 4 part 1

---
+++ 2023/day4/main.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+import pathlib
+import re
+import unittest
+import sys
+
+BASE_DIR = pathlib.Path(__file__).parent
+CARD_PATTERN = re.compile(r"Card\s+(\d+)")
+
+
+class Tests(unittest.TestCase):
+    def test_example(self):
+        input: list[str] = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+""".splitlines()
+        actual = main(input)
+        expected = [8, 2, 2, 1, 0, 0]
+        assert actual == expected
+
+    def test_empty(self):
+        assert main([]) == []
+    
+
+def main(input: list[str]) -> list[int]:
+    solution: list[int] = []
+
+    for line in input:
+        left, card_str = tuple(line.split("|", maxsplit=1))
+        card_id_str, winning_str = left.split(":", maxsplit=1)
+        
+        winning_numbers = [int(num) for num in winning_str.split()]
+        card_numbers = [int(num) for num in card_str.split()]
+        
+        card_id = -1
+        card_match = CARD_PATTERN.match(card_id_str)
+        if card_match:
+            card_id = int(card_match.groups()[0])
+        
+        matching_numbers = set(winning_numbers) & set(card_numbers)
+        if not matching_numbers:
+            solution.append(0)
+            continue
+    
+        power = len(matching_numbers) - 1
+        points = 2 ** power
+        solution.append(points)
+
+    return solution
+
+
+if __name__ == "__main__":
+    path = pathlib.Path(sys.argv[1])
+
+    input: list[str]
+    if path.exists():
+        input = path.read_text().splitlines()
+    else:
+        input = sys.argv[1:]
+
+    solution = main(input)
+    print(sum(solution))
